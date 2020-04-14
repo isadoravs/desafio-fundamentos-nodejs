813deb8891b31816d474ab8e941f7a1c5931943b
Isadora Vasconcellos <vs.isadora@gmail.com>
2020-04-14 19:53:44 -0300

    Initial commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  type: 'income' | 'outcome';
+  title: string;
+  value: number;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    balance = this.transactions.reduce((accumulated, transaction) => {
+      switch (transaction.type) {
+        case 'income':
+          return {
+            ...accumulated,
+            income: transaction.value + accumulated.income,
+            total: accumulated.income - accumulated.outcome + transaction.value,
+          };
+        case 'outcome':
+          return {
+            ...accumulated,
+            outcome: transaction.value + accumulated.outcome,
+            total: accumulated.income - accumulated.outcome - transaction.value,
+          };
+        default:
+          return accumulated;
+      }
+    }, balance);
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ type, title, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ type, title, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
